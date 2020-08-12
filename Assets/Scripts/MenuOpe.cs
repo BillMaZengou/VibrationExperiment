abb35afbf2ec2497bce71ff3b85b3177f263b947
Bill Ma <mazengou@gmail.com>
2020-08-12 18:41:09 +0800

    Ex2 menu works. Ex3 UIHelp doesn't TODO: Debug Ex3 UI. Make menu appear or disappear

---
+++ Assets/Scripts/MenuOpe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOpe : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnResumeClick()
+    {
+    }
+
+    public void OnEx2Click()
+    {
+        Debug.Log("Scene2 loading: " + "Ex2");
+        SceneManager.LoadScene("Ex2", LoadSceneMode.Single);
+    }
+
+    public void OnEx3Click() {
+        Debug.Log("Scene2 loading: " + "Ex3");
+        SceneManager.LoadScene("Ex34", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public GameObject Menu;
+    public OVRInput.Button toggleButtonforVR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
